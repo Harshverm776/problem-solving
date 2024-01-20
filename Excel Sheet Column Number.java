f76f3e8d1f4cca14c983462c5453ecf37d8f60fc
Harsh Verma <47974733+Harshverm776@users.noreply.github.com>
2024-01-20 12:54:10 +0530

    Update Excel Sheet Column Number.java

--- Excel Sheet Column Number.java
+++ Excel Sheet Column Number.java
@@ -18,3 +18,19 @@ class Solution {
         return decimal;
     }
 }
+
+// Different Approach - Similar to above only but difference in calculation
+// result = 26*result + s[i] - 'A' + 1
+
+static int titleToNumber(String s)
+{
+    // This process is similar to 
+    // binary-to-decimal conversion
+    int result = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        result *= 26;
+        result += s.charAt(i) - 'A' + 1;
+    }
+    return result;
+}
